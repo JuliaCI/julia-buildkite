7401bf27a4effceb50cbc20db1b5c86d87d5e395
Dilum Aluthge <dilum@aluthge.com>
2022-01-09 22:05:19 -0500

    Add the `rr_invoke.jl` script as a convenient wrapper for running the Yggdrasil `rr` binary (#20)

---
+++ utilities/rr/rr_invoke.jl
@@ -0,0 +1,24 @@
+# Usage:    julia rr_invoke.jl [args...]
+#
+# Instead of running `rr foo bar baz ...`, you would run `julia rr_invoke.jl foo bar baz ...`
+# instead.
+# 
+# Examples: julia rr_invoke.jl --version
+#           julia rr_invoke.jl --help
+#           julia rr_invoke.jl replay ...
+
+import Pkg
+
+# We add `rr_jll` to a temporary project.
+# Note: we still use the user's default depot paths. If the user does not want to use their
+# default depot paths, they should do e.g. `export JULIA_DEPOT_PATH=$(mktemp -d)` before
+# running the `rr_invoke.jl` script.
+Pkg.activate(mktempdir(; cleanup = true))
+
+Pkg.add(Pkg.PackageSpec(name = "rr_jll", version = v"5.5.0"))
+
+import rr_jll
+
+rr_jll.rr() do rr_path
+    run(`$(rr_path) $ARGS`)
+end
