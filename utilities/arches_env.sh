98b767e85ab66d562d2a3b3cf5a0388b3207c38c
Elliot Saba <staticfloat@gmail.com>
2022-03-09 08:14:28 -0800

    Enable `x86_64-linux-gnu` packager (#42)
    * Enable `x86_64-linux-gnu` packager with new rootfs
    
    Use new rootfs image with custom-built glibc and gcc version.
    
    * Remove this from the grouping for now due to cryptic requirements

---
+++ utilities/arches_env.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+set -eou pipefail
+
+# This script reads in an `.arches` file, processes the columns and default value mappings
+# within it, and outputs an environment block (e.g. a line of the form "X=a B= C=123") for
+# each architecture defined within, to be used by other tools such as the brother script
+# `arches_pipeline_upload.sh`, which uses those environment mappings to template pipeline
+# YAML files that are being uploaded by `buildkite-agent pipeline upload`.
+
+ARCHES_FILE="${1:-}"
+if [[ ! -f "${ARCHES_FILE}" ]] ; then
+    echo "Arches file does not exist: '${ARCHES_FILE}'"
+    exit 1
+fi
+
+enforce_sanitized() {
+    for value in "$@"; do
+        if ! [[ "${value}" =~ ^[a-zA-Z0-9_\.[:space:]-]*$ ]]; then
+            echo "Arches file '${ARCHES_FILE}' contains value '${value}' with non-alphanumeric characters; refusing to parse!" >&2
+            exit 1
+        fi
+    done
+}
+
+
+# Determine variable names from the header of the .arches file:
+readarray -d ' ' -s 1 -t var_names < <(head -1 "${ARCHES_FILE}" | tr -s ' ')
+
+# Determine any embedded defaults
+declare -A defaults_map
+readarray -t default_mapping_lines < <(grep "^#default" "${ARCHES_FILE}" | tr -s ' ')
+for idx in "${!default_mapping_lines[@]}"; do
+    key="$(  cut -d' ' -f2 <<<"${default_mapping_lines[${idx}]}" | xargs)"
+    value="$(cut -d' ' -f3 <<<"${default_mapping_lines[${idx}]}" | xargs)"
+    enforce_sanitized "${key}" "${value}"
+    defaults_map["${key}"]="${value}"
+done
+
+cat "${ARCHES_FILE}" | while read line; do
+    # Remove whitespace from the beginning and end of each line
+    line="$(tr -s ' ' <<<"${line}")"
+
+    # Skip any line that begins with the `#` character
+    if [[ $line == \#* ]]; then
+        continue
+    fi
+
+    # Skip any empty line
+    if [[ $line == "" ]]; then
+        continue
+    fi
+
+    # Skip any line that contains suspicious characters, to prevent shell escaping bugs
+    enforce_sanitized "${line}"
+
+    # Convert line to array
+    readarray -d ' ' -t line_array <<<"${line}"
+
+    # Panic if we don't have the same number of items as our column names:
+    if [[ "${#line_array[@]}" != "${#var_names[@]}" ]]; then
+        echo "ERROR: The following line does not contain ${#var_names[@]} columns as we would expect from the header of ${ARCHES_FILE}" >&2
+        echo "${line}"
+        exit 1
+    fi
+
+    # Loop over columns, bind values to their column name
+    for idx in "${!var_names[@]}"; do
+        # Get the name and value
+        name="$(xargs <<<"${var_names[${idx}]}")"
+        value="$(xargs <<<"${line_array[${idx}]}")"
+
+        # Apply default values to our special `.` token
+        if [[ "${value}" == "." ]]; then
+            value="${defaults_map[${name}]:-}"
+        fi
+
+        echo -n "${name}=\"${value}\" "
+    done
+    echo
+done
---
+++ utilities/calc_version_envs.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Extract information from our triplet
+export ARCH="$(cut -d- -f1 <<<"${TRIPLET}")"
+case "${TRIPLET}" in
+    *-apple-*)
+        OS="macos"
+        ;;
+    *-freebsd*)
+        OS="freebsd"
+        ;;
+    *-mingw*)
+        OS="windows"
+        ;;
+    *-gnu*)
+        OS="linux"
+        ;;
+    *-musl*)
+        OS="musl"
+        ;;
+    *)
+        echo "Unknown triplet OS '${TRIPLET}'" >&2
+        exit 1
+        ;;
+esac
+export OS
+
+# Extract git information
+SHORT_COMMIT_LENGTH=10
+LONG_COMMIT="$(git rev-parse HEAD)"
+SHORT_COMMIT="$(echo ${LONG_COMMIT} | cut -c1-${SHORT_COMMIT_LENGTH})"
+
+# Extract information about the current julia version number
+JULIA_VERSION="$(cat VERSION)"
+MAJMIN="${JULIA_VERSION:0:3}"
+
+# If we're on a tag, then our "tar version" will be the julia version.
+# Otherwise, it's the short commit.
+if git describe --tags --exact-match >/dev/null 2>/dev/null; then
+    TAR_VERSION="${JULIA_VERSION}"
+else
+    TAR_VERSION="${SHORT_COMMIT}"
+fi
+
+# Build the filename that we'll upload as, and get the filename that will be built
+# These are not the same in situations such as `musl`, where the build system doesn't
+# differentiate but we need to give it a different name.
+JULIA_BINARYDIST_FILENAME="$(make print-JULIA_BINARYDIST_FILENAME | cut -c27- | tr -s ' ').tar.gz"
+
+JULIA_INSTALL_DIR="julia-${TAR_VERSION}"
+JULIA_BINARY="${JULIA_INSTALL_DIR}/bin/julia"
+
+# We generally upload to multiple upload targets
+UPLOAD_TARGETS=(
+    # First, we have the canonical fully-specified upload target
+    "julialangnightlies/bin/${OS?}/${ARCH?}/${MAJMIN?}/julia-${TAR_VERSION?}-${OS?}-${ARCH?}.tar.gz"
+
+    # Next, we have the "latest" upload target
+    "julialangnightlies/bin/${OS?}/${ARCH?}/${MAJMIN?}/julia-latest-${OS?}-${ARCH?}.tar.gz"
+)
+UPLOAD_FILENAME="julia-${TAR_VERSION?}-${OS?}-${ARCH?}.tar.gz"
+
+# Finally, for compatibility, we keep on uploading x86_64 and i686 targets to folders called `x64`
+# and `x86`, respectively, although I would very much like to stop doing that.
+if [[ "${ARCH}" == "x86_64" ]]; then
+    UPLOAD_TARGETS+=( "julialangnightlies/bin/${OS?}/x64/${MAJMIN?}/julia-${TAR_VERSION?}-${OS?}-${ARCH?}.tar.gz" )
+    UPLOAD_TARGETS+=( "julialangnightlies/bin/${OS?}/x64/${MAJMIN?}/julia-latest-${OS?}-${ARCH?}.tar.gz" )
+elif [[ "${ARCH}" == "i686" ]]; then
+    UPLOAD_TARGETS+=( "julialangnightlies/bin/${OS?}/x86/${MAJMIN?}/julia-${TAR_VERSION?}-${OS?}-${ARCH?}.tar.gz" )
+    UPLOAD_TARGETS+=( "julialangnightlies/bin/${OS?}/x86/${MAJMIN?}/julia-latest-${OS?}-${ARCH?}.tar.gz" )
+fi
+
+echo "--- Print the full and short commit hashes"
+echo "The full commit is:                      ${LONG_COMMIT}"
+echo "The short commit is:                     ${SHORT_COMMIT}"
+echo "Julia will be installed to:        ${JULIA_BINARY}"
+echo "Detected Julia version:            ${MAJMIN}  (${JULIA_VERSION})"
+echo "Detected build platform:           ${TRIPLET}  (${ARCH}, ${OS})"
+echo "Julia will be uploaded to:         s3://${UPLOAD_TARGETS[0]}"
+echo "With additional upload targets:"
+for UPLOAD_TARGET in ${UPLOAD_TARGETS[@]:1}; do
+    echo " -> s3://${UPLOAD_TARGET}"
+done
