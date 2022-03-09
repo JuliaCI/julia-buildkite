ae7eb0abc7fefe21b5c217c5cddd241121949b65
Elliot Saba <staticfloat@gmail.com>
2022-03-09 12:36:30 -0800

    Add GPG signing to linux tarball uploads

---
+++ utilities/sign_tarball.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TARGET="${2}"
+
+# Create temporary gpg home so that we have an isolated keyring
+export GNUPGHOME="$(mktemp -d)"
+trap 'rm -rf "$GNUPGHOME"' EXIT INT TERM HUP
+
+# Import key as first argument
+gpg --import <"${1}"
+
+# Sign the second argument as a file, creating a `.asc` file.
+gpg --armor --detach-sig --batch --yes "${2}"
