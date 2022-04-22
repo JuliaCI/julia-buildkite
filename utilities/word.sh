490db1290444353d99a4fd8addf37df61250ec4a
Dilum Aluthge <dilum@aluthge.com>
2022-04-22 01:14:14 -0400

    Add a few quick sanity checks after building (#112)

---
+++ utilities/word.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+case "${ARCH}" in
+    x86_64)
+        EXPECTED_WORD_SIZE="64"
+        ;;
+    i686)
+        EXPECTED_WORD_SIZE="32"
+        ;;
+    aarch64)
+        EXPECTED_WORD_SIZE="64"
+        ;;
+    armv7l)
+        EXPECTED_WORD_SIZE="32"
+        ;;
+    powerpc64le)
+        EXPECTED_WORD_SIZE="64"
+        ;;
+    # fallback
+    *)
+        echo "Unknown arch '${ARCH}'" >&2
+        exit 1
+        ;;
+esac
+export EXPECTED_WORD_SIZE
