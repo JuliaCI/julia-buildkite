d9eb813c8517b9e4903bff003aad5cdc3f214274
Elliot Saba <staticfloat@gmail.com>
2022-06-07 16:06:59 -0700

    Notarize all macOS builds
    This adapts the logic from `contrib/mac/app` in the main Julia build
    tree to notarize `.dmg`'s using encrypted credentials.

--- utilities/macos/build_dmg.sh
+++ utilities/macos/build_dmg.sh
@@ -38,18 +38,70 @@ cp -aR "${JULIA_INSTALL_DIR?}" "${APP_PATH}/Contents/Resources/julia"
 
 # Create `.dmg`.  We create it with 1TB size, but since that is
 # a maximum, it has no effect on download or unpack size.
-hdiutil create \
-    "${DMG_NAME}" \
-    -size 1t \
-    -fs HFS+ \
-    -volname "Julia-${TAR_VERSION?}" \
-    -imagekey zlib-level=9 \
-    -srcfolder "${DMG_PATH}"
+# We define this in a function because we need to do it again later.
+function create_dmg() {
+    hdiutil create \
+        "${DMG_NAME}" \
+        -size 1t \
+        -fs HFS+ \
+        -volname "Julia-${TAR_VERSION?}" \
+        -imagekey zlib-level=9 \
+        -srcfolder "${DMG_PATH}"
 
-# Sign the `.dmg` itself
-.buildkite/utilities/macos/codesign.sh \
-    --keychain "${KEYCHAIN_PATH}" \
-    --identity "${MACOS_CODESIGN_IDENTITY}" \
-    "${DMG_NAME}"
+    # Sign the `.dmg` itself
+    .buildkite/utilities/macos/codesign.sh \
+        --keychain "${KEYCHAIN_PATH}" \
+        --identity "${MACOS_CODESIGN_IDENTITY}" \
+        "${DMG_NAME}"
+}
+create_dmg
 
-rm -rf "${DMG_PATH}"
+# Upload the `.dmg` for notarization
+xcrun altool \
+    --notarize-app \
+    --primary-bundle-id org.julialang.launcherapp \
+    --username "${NOTARIZATION_APPLE_ID}" \
+    --password "${NOTARIZATION_APPLE_KEY}" \
+    -itc_provider A427R7F42H \
+    --file "${DMG_NAME}" \
+    --output-format xml > notarization.xml
+
+# Get the upload UUID from the xml file
+UUID="$(/usr/libexec/PlistBuddy -c "print notarization-upload:RequestUUID" notarization.xml 2>/dev/null)"
+echo "Waiting until UUID ${UUID} is done processing...."
+
+# Wait for apple's servers to give us a valid notarization
+while true; do
+    xcrun altool \
+        --notarization-info "${UUID}" \
+        --username "${NOTARIZATION_APPLE_ID}" \
+        --password "${NOTARIZATION_APPLE_KEY}" \
+        --output-format xml > notarization.xml
+    STATUS=$(/usr/libexec/PlistBuddy -c "print notarization-info:Status" notarization.xml 2>/dev/null)
+
+    # Process loop exit conditions
+    if [[ ${STATUS} == "success" ]]; then
+        echo "Notarization finished"
+        break
+    elif [[ ${STATUS} == "in progress" ]]; then
+        echo -n "."
+        sleep 10
+        continue
+    elif [[ ${STATUS} == "invalid" ]]; then
+        echo "invalid!  Looks like something got borked:"
+        /usr/libexec/PlistBuddy -c "print notarization-info:LogFileURL" notarization.xml 2>/dev/null
+        exit 1
+    else
+        echo "Notarization failed with status ${STATUS}"
+        exit 1
+    fi
+done
+
+# Staple the notarization to the app
+xcrun stapler staple "${APP_PATH}"
+
+# Re-build the .dmg from the app now that it's notarized
+create_dmg
+
+# Cleanup things we created here
+rm -rf "${DMG_PATH}" notarization.xml
