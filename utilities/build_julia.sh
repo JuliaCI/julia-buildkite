c78a436dac6beff27cfc7c62c157ab6bdfc7287f
Dilum Aluthge <dilum@aluthge.com>
2022-11-30 20:01:48 -0500

    Linux: on every PR, run `make -C deps USE_BINARYBUILDER=0` and make sure that the working directory is clean

--- utilities/build_julia.sh
+++ utilities/build_julia.sh
@@ -47,14 +47,7 @@ done
 echo "--- Build Julia"
 make "${MFLAGS[@]}"
 
-
-echo "--- Check that the working directory is clean"
-if [ -n "$(git status --short)" ]; then
-    echo "ERROR: The working directory is dirty." >&2
-    echo "Output of git status:" >&2
-    git status
-    exit 1
-fi
+source .buildkite/utilities/check_clean_wd.sh
 
 echo "--- Print Julia version info"
 # use `JULIA_BINARY` since it has the `.exe` extension already determined,
---
+++ utilities/check_clean_wd.sh
@@ -0,0 +1,7 @@
+echo "--- Check that the working directory is clean"
+if [ -n "$(git status --short)" ]; then
+    echo "ERROR: The working directory is dirty." >&2
+    echo "Output of git status:" >&2
+    git status
+    exit 1
+fi
