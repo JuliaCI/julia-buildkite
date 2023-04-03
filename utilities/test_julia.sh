2870d8801a0567b469ee6b29af049e9da9d0eeaf
Ian <i.r.butterworth@gmail.com>
2023-04-02 22:07:07 -0400

    run after codesigning before tests

--- utilities/test_julia.sh
+++ utilities/test_julia.sh
@@ -30,7 +30,10 @@ fi
 # If we're on macOS, we need to re-sign the downloaded tarball so it will
 # execute on this machine
 if [[ "${OS}" == "macos" ]]; then
+    echo "--- [mac] Codesigning"
     .buildkite/utilities/macos/codesign.sh "${JULIA_INSTALL_DIR}"
+    echo "--- [mac] Update checksums for stdlib cachefiles after codesigning"
+    ${JULIA_INSTALL_DIR}/bin/julia .buildkite/utilities/macos/update_stdlib_pkgimage_checksums.jl
 fi
 
 
