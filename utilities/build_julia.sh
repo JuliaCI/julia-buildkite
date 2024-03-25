fa9db4b3a293189e6e21d0a421faa1ea858a2f11
Zentrik <Zentrik@users.noreply.github.com>
2024-03-25 20:38:45 +0000

    Add building and testing of PGO+LTO LLVM and Julia

--- utilities/build_julia.sh
+++ utilities/build_julia.sh
@@ -19,7 +19,6 @@ ld -v
 echo
 buildkite-agent --version
 
-echo "--- Collect make options"
 # These are the flags we'll provide to `make`
 MFLAGS=()
 
@@ -36,17 +35,43 @@ MFLAGS+=( "VERBOSE=1" )
 MFLAGS+=( "TAGGED_RELEASE_BANNER=Official https://julialang.org/ release" )
 MFLAGS+=( "JULIA_CPU_TARGET=${JULIA_CPU_TARGET}" )
 
+if [[ ! -z "${USE_JULIA_PGO_LTO-}" ]]; then
+    MFLAGS+=( "STAGE2_BUILD=$PWD/usr" )
+
+    echo "--- Collect make options"
+    echo "Make Options:"
+    for FLAG in "${MFLAGS[@]}"; do
+        echo " -> ${FLAG}"
+    done
+
+    echo "--- Build Julia Stage 1 - with instrumentation"
+
+    cd contrib/pgo-lto
+    ${MAKE} "${MFLAGS[@]}" stage1
+
+    echo "--- Collecting Profile"
+    ${MAKE} clean-profiles
+    "./stage1.build/julia .buildkite/utilities/pgo_script.jl"
+fi
+
 # Finish off with any extra make flags from the `.arches` file
 MFLAGS+=( $(tr "," " " <<<"${MAKE_FLAGS}") )
 
+echo "--- Collect make options"
 echo "Make Options:"
 for FLAG in "${MFLAGS[@]}"; do
     echo " -> ${FLAG}"
 done
 
-echo "--- Build Julia"
-${MAKE} "${MFLAGS[@]}"
+if [[ ! -z "${USE_JULIA_PGO_LTO-}" ]]; then
+    echo "--- Build Julia Stage 2 - optimised"
+    ${MAKE} "${MFLAGS[@]}" stage2
 
+    cd ../..
+else
+    echo "--- Build Julia"
+    ${MAKE} "${MFLAGS[@]}"
+fi
 
 echo "--- Check that the working directory is clean"
 if [ -n "$(git status --short)" ]; then
