a26c8074488b306e7d4a7ad890622aba7d6c72f5
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-08-18 12:10:44 +0000

    Add bug in v1.9 in exchange for readability
    Co-authored-by: Elliot Saba <staticfloat@gmail.com>

--- utilities/build_julia.sh
+++ utilities/build_julia.sh
@@ -33,7 +33,8 @@ MFLAGS+=( "-j${JULIA_CPU_THREADS}")
 
 # Add a few default flags to our make flags:
 MFLAGS+=( "VERBOSE=1" )
-if grep '1\.\(1[2-9]\|[2-9][0-9]\|[0-9][0-9][0-9][0-9]*\)\.' VERSION >/dev/null; then # if version >= 1.12
+# NOTE: this breaks if you build v1.9 or lower with this
+if awk "BEGIN {exit !($d1 >= 1.12)}"; then
     MFLAGS+=( "TAGGED_RELEASE_BANNER=Official https://julialang.org release" )
 else
     # Keep trailing slash for compatability. The slash was removed in 1.12 with https://github.com/JuliaLang/julia/pull/53978
