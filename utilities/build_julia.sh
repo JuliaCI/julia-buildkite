403e6f2cffd0ac95ce8ac3a417a3d1696dab1f31
Zentrik <llvm.zentrik@gmail.com>
2025-10-10 15:50:31 +0100

    Draft PGO+LTO+BOLT build + test CI

--- utilities/build_julia.sh
+++ utilities/build_julia.sh
@@ -26,7 +26,6 @@ if [[ "${ROOTFS_IMAGE_NAME-}" == "llvm_passes" ]]; then
     contrib/download_cmake.sh
 fi
 
-echo "--- Collect make options"
 # These are the flags we'll provide to `make`
 MFLAGS=()
 
@@ -55,19 +54,57 @@ else
 fi
 MFLAGS+=( "JULIA_CPU_TARGET=${JULIA_CPU_TARGET}" )
 
+if [[ ! -z "${USE_JULIA_PGO_LTO_BOLT-}" ]]; then
+    MFLAGS+=( "STAGE2_BUILD=$PWD" )
+
+    echo "--- Collect make options"
+    echo "Make Options:"
+    for FLAG in "${MFLAGS[@]}"; do
+        echo " -> ${FLAG}"
+    done
+
+    echo "--- Build Julia Stage 1 - with instrumentation"
+
+    cd contrib/pgo-lto-bolt
+    ${MAKE} "${MFLAGS[@]}" stage1
+    # Building stage1 collects profiling data which we use instead of collecting our own
+fi
+
 # Finish off with any extra make flags from the `.arches` file
 IFS=',' read -ra ARCHES_FLAGS <<<"${MAKE_FLAGS}"
 MFLAGS+=( "${ARCHES_FLAGS[@]}" )
 
+echo "--- Collect make options"
 echo "Make Options:"
 for FLAG in "${MFLAGS[@]}"; do
     echo " -> ${FLAG}"
 done
 
-echo "--- Build Julia"
-echo "Note: The log stream is filtered. [buildroot] replaces pwd $(pwd)"
-${MAKE} "${MFLAGS[@]}" 2>&1 | sed "s|$(pwd)|[buildroot]|g"
+if [[ ! -z "${USE_JULIA_PGO_LTO_BOLT-}" ]]; then
+    echo "--- Build Julia Stage 2 - PGO + LTO optimised"
+    ${MAKE} "${MFLAGS[@]}" stage2
 
+    echo "--- Copying original shared libraries"
+    ${MAKE} "${MFLAGS[@]}" copy_originals
+
+    echo "--- Instrumenting with BOLT"
+    ${MAKE} "${MFLAGS[@]}" bolt_instrument
+
+    echo "--- Finishing and Profiling Julia Stage 2"
+    ${MAKE} "${MFLAGS[@]}" finish_stage2
+
+    echo "--- Merging BOLT Profiles"
+    ${MAKE} "${MFLAGS[@]}" merge_data
+
+    echo "--- Optimize Julia Stage 2 with BOLT"
+    ${MAKE} "${MFLAGS[@]}" bolt
+
+    cd ../..
+else
+    echo "--- Build Julia"
+    echo "Note: The log stream is filtered. [buildroot] replaces pwd $(pwd)"
+    ${MAKE} "${MFLAGS[@]}" 2>&1 | sed "s|$(pwd)|[buildroot]|g"
+fi
 
 echo "--- Check that the working directory is clean"
 if [ -n "$(git status --short)" ]; then
@@ -101,3 +138,9 @@ fi
 
 echo "--- Upload build artifacts to buildkite"
 buildkite-agent artifact upload "${UPLOAD_FILENAME}.tar.gz"
+
+# Upload the profile data to allow for reproducible builds
+if [[ ! -z "${USE_JULIA_PGO_LTO_BOLT-}" ]]; then
+    buildkite-agent artifact upload "contrib/pgo-lto-bolt/profiles/merged.prof"
+    buildkite-agent artifact upload "contrib/pgo-lto-bolt/profiles-bolt/*.merged.fdata"
+fi
