fe6f0d0bf96d34598f85d71c0ad0b113ace1fd35
Ian <i.r.butterworth@gmail.com>
2026-02-01 09:34:30 -0500

    coverage: use the checked out JuliaSyntax version

--- pipelines/scheduled/coverage/upload_coverage.jl
+++ pipelines/scheduled/coverage/upload_coverage.jl
@@ -4,6 +4,7 @@ import Pkg
 import Logging
 import TOML
 
+Pkg.develop(path="./JuliaSyntax")
 Pkg.add(; name = "Coverage", uuid = "a2441757-f6aa-5fb2-8edb-039e3f45d037", version = "1")
 
 import Coverage
